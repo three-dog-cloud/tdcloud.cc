e4e14ce886ccec542d3d3aee9067f71db27caff4
tdclouds <tdclouds@users.noreply.github.com>
2024-04-19 07:26:41 +0000

    deploy: 3779e86c6cba0d844143eada5a8f36af90728647

---
+++ assets/good-BowSc02q.js
@@ -0,0 +1,1 @@
+import{N as t}from"./index-DOwwAGMQ.js";function e(o){return t({url:"/api/good",params:o})}function a(o){return t({url:`/api/good/${o}`})}export{a,e as g};
--- assets/good-DR_1IUh6.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./index--vmemoQz.js";function e(o){return t({url:"/api/good",params:o})}function a(o){return t({url:`/api/good/${o}`})}export{a,e as g};
