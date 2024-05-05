97934d9ceb3fc06edb50d41e951a35564e3fdb17
tdclouds <tdclouds@users.noreply.github.com>
2024-05-05 17:10:45 +0000

    deploy: f94bd802f43dad1fa6dd161893eb6a0d451c922b

--- C_89h7h9.js
+++
@@ -1,1 +0,0 @@
-import{I as t}from"./assets/index-B7U8UwVH.js";function e(o){return t({url:"/api/good",params:o})}function a(o){return t({url:`/api/good/${o}`})}export{a,e as g};
---
+++ xLa1wxrZ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./DlAUqK2U.js";const r={};function c(n,t){return"FAQ"}const o=e(r,[["render",c]]);export{o as default};
