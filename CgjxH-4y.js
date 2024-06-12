1affb4f3134cd1a0b8f7e8106ca9d8929525c101
tdclouds <tdclouds@users.noreply.github.com>
2024-06-12 16:11:42 +0000

    deploy: a57780572386759a04eabb1cb78737982e763026

---
+++ CgjxH-4y.js
@@ -0,0 +1,1 @@
+import{L as t}from"./assets/index-Cw9-CrQ3.js";function e(o){return t({url:"/api/good",params:o})}function a(o){return t({url:`/api/good/${o}`})}export{a,e as g};
--- qAF0HmQs.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./DlAUqK2U.js";import{a as t,b as s,q as n,w as e,e as c,f as _}from"./assets/index-aeLSw1Ag.js";const f={};function p(u,i){const o=t("a-button"),a=t("a-result");return s(),n(a,{status:"404",title:"404","sub-title":"此页面未找到。"},{extra:e(()=>[c(o,{type:"primary",href:"/"},{default:e(()=>[_("首页")]),_:1})]),_:1})}const d=r(f,[["render",p]]);export{d as default};
